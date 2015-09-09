ce65d1d028051acb38832b1d991eb1423f4cb9bc
Pierre-Luc Babin <plbabin@gmail.com>
2015-09-09 13:19:01 +0000

    Add homebrew export of current installed formula

---
+++ homebrew.sh
@@ -0,0 +1,158 @@
+#================================================================================
+
+brew update
+
+# brew tap homebrew/dupes
+# brew tap homebrew/versions
+# brew tap caskroom/cask
+# brew tap caskroom/fonts
+# brew tap caskroom/versions
+# brew tap josegonzalez/homebrew-php
+# brew uni
+
+
+# brew install findutils
+# brew install ringojs
+# brew install narwhal
+# brew install ack
+# brew install autoconf
+# brew install automake
+# brew install bash
+# brew install cairo
+# brew install cmake
+# brew install composer
+# brew install ctags
+# brew install curl --with-ssh
+# brew install curl-ca-bundle
+# brew install faac
+# brew install ffmpeg
+# brew install fontconfig
+# brew install freetype
+# brew install gettext
+# brew install git
+# brew install git-ftp
+# brew install glib
+# brew install gmp
+# brew install gnutls
+# brew install grep
+# brew install icu4c
+# brew install imagemagick
+# brew install ios-webkit-debug-proxy
+# brew install jpeg
+# brew install lame
+# brew install libevent
+# brew install libffi
+# brew install libgcrypt
+# brew install libgpg-error
+# brew install libiconv
+# brew install libimobiledevice
+# brew install libmemcached
+# brew install libplist
+# brew install libpng
+# brew install libssh2
+# brew install libtasn1
+# brew install libtool
+# brew install libusb
+# brew install libxml2
+# brew install libxslt
+# brew install libyaml
+# brew install lynx
+# brew install mcrypt
+# brew install memcached
+# brew install mongodb
+# brew install most
+# brew install mysql
+# brew install neon
+# brew install nettle
+# brew install nginx
+# brew install node
+# brew install openssl
+# brew install ossp-uuid
+# brew install p11-kit
+# brew install pcre
+# brew install php53-mcrypt
+# brew install php53-memcache
+# brew install php53-memcached
+# brew install php54 --with-mysql --with-fpm
+# brew install pixman
+# brew install pkg-config
+# brew install rbenv
+# brew install redis
+# brew install ruby-build
+# brew install s-lang
+# brew install serf
+# brew install sqlite
+# brew install ssh-copy-id
+# brew install subversion
+# brew install texi2html
+# brew install tig
+# brew install tree
+# brew install unixodbc
+# brew install unrar
+# brew install usbmuxd
+# brew install wget --enable-iri
+# brew install x264
+# brew install xvid
+# brew install xz
+# brew install yasm
+# brew install zlib
+# brew install homebrew/dupes/grep
+# brew install readline && CONFIGURE_OPTS="--with-readline-dir=$(--prefix readline)"
+# brew install postgresql
+# brew install trash
+	
+#echo "to update setuptools & pip run: pip --upgrade setuptools pip --upgrade pip"
+#	echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+#echo " Changing shell...."
+#sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
+
+#brew cask install alfred
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install iterm2
+brew cask install rdio
+brew cask install sketch 
+brew cask install sequel-pro
+brew cask install dash
+brew cask install hipchat
+brew cask install skype
+brew cask install onepassword
+brew cask install flux-beta
+brew cask install evernote
+brew cask install spectacle
+brew cask install karabiner
+brew cask install hazel
+brew cask install fantastical
+brew cask install cloud
+brew cask install backblaze
+brew cask install boom
+brew cask install xtrafinder
+brew cask install steam
+brew cask install doxie
+brew cask install appcleaner
+brew cask install caffeine
+brew cask install charles
+brew cask install cleanmymac
+brew cask install diffmerge
+brew cask install divvy
+brew cask install fitbit-connect
+brew cask install joinme
+brew cask install postgres
+brew cask install pg-commander
+brew cask install private-internet-access
+brew cask install quickcast
+brew cask install subsmarine
+# brew cask install teamspeak-client
+brew cask install vlc
+brew cask install vmware-fusion
+brew cask install superduper
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install the-unarchiver
+brew cask install virtualbox
+brew cask install sparrow
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install firefox
+	
+brew cleanup
